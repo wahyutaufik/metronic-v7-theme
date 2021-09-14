201e190ea9c55cc1f3631d58d825f40dc1cfd7a0
Wahyu Taufik <wahyutaufik37@gmail.com>
2021-09-14 17:09:33 +0700

    Fixing datepicker schema

--- templates/_schema/date_picker/input.blade.php
+++ templates/_schema/date_picker/input.blade.php
@@ -1,5 +1,6 @@
-<div class="input-group date" date-format="{{ $self->date_format }}"  data-value="{{ $value }}">
-    <input type="text" class="form-control datepicker_modal"  name="{{ $self['name'] }}" value="{{ $value }}"/>
+<div class="input-group date date-picker-bono" date-format="{{ $self->date_format }}" data-value="{{ $value }}">
+    <input type="hidden" name="{{ $self['name'] }}" value="{{ $value }}" />
+    <input type="text" class="form-control" >
     <div class="input-group-append">
         <button class="btn btn-primary" type="button">
             <i class="la la-calendar"></i>
